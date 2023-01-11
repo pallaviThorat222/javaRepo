7cc65d96c567c2e96daa1a4f8441bdadc9efd3f6
unknown <pallavithorat@gmail.com>
2023-01-11 09:48:47 +0530

    added doProcess

--- src/main/java/itsolutions/demo.java
+++ src/main/java/itsolutions/demo.java
@@ -1,5 +1,33 @@
 package itsolutions;
 
 public class demo {
+	private Integer empId;
+	private String empName;
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	@Override
+	public String toString() {
+		return "demo [empId=" + empId + ", empName=" + empName + "]";
+	}
+	public demo() {
+		// TODO Auto-generated constructor stub
+	}
+	public demo(Integer empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+	
+	
 
 }
